79dc6215d8f6dd9e391b23c47c4b84cdc4b56034
Hardeep Asrani <hardeepasrani@gmail.com>
2018-07-03 07:23:37 +0000

    Added read more link
    Fixes:https://github.com/Codeinwp/amadeus/issues/69

--- functions.php
+++ functions.php
@@ -295,6 +295,16 @@ function amadeus_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'amadeus_excerpt_length', 999 );
 
+/**
+ * Excerpt more link
+ */
+function amadeus_excerpt_more( $more ) {
+    return sprintf( '&hellip;<a href="%1$s" class="more-link">%2$s</a>',
+          esc_url( get_permalink( get_the_ID() ) ),
+          sprintf( __( 'Continue reading %s', 'amadeus' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
+    );
+}
+add_filter( 'excerpt_more', 'amadeus_excerpt_more' );
 
 /**
  * Footer credits
